f37a327a68c86edf11f6c82557337c9c8ce11a12
MikhailSorokin <sorokinmike@yahoo.com>
2018-12-02 05:47:08 -0500

    Attempt at running this code.

--- mold_detector.py
+++ mold_detector.py
@@ -4,6 +4,7 @@ from google.cloud.vision import types
 
 import numpy as np
 import argparse
+import requests
 
 def getFungusStatus(labels):
 	status = 0
@@ -13,6 +14,23 @@ def getFungusStatus(labels):
 			status = 1
 
 	return status
+
+
+
+def create_ImageRequest(bStatus, colorList):
+
+	for each text in textDescriptors:
+
+	headers = {'Status': 'Fungus', 'Colors': 'colors'}
+
+    makeImageRequest = requests.post('https://slewando.wixsite.com/',
+	auth=('user', 'pass'), data = bStatus)
+
+	print(makeImageRequest.url)
+	#makeImageRequest = requests.post('https://slewando.wixsite.com/', data = {'key': 'val'})
+
+    #print(makeImageRequest.url)
+# [END create_instance]
 
 if __name__ == '__main__':
 	ap = argparse.ArgumentParser()
@@ -46,79 +64,3 @@ if __name__ == '__main__':
 			colorList.append(color.color)
 
 	print(colorList)
-
-'''
-def create_instance(compute, project, zone, name, bucket):
-    # Get the latest Debian Jessie image.
-    image_response = compute.images().getFromFamily(
-        project='debian-cloud', family='debian-9').execute()
-    source_disk_image = image_response['diskTwo']
-
-    # Configure the machine
-    machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
-    startup_script = open(
-        os.path.join(
-            os.path.dirname(__file__), 'startup-script.sh'), 'r').read()
-    image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
-    image_caption = "Ready for dessert?"
-
-    config = {
-        'name': name,
-        'machineType': machine_type,
-
-        # Specify the boot disk and the image to use as a source.
-        'disks': [
-            {
-                'boot': True,
-                'autoDelete': True,
-                'initializeParams': {
-                    'sourceImage': source_disk_image,
-                }
-            }
-        ],
-
-        # Specify a network interface with NAT to access the public
-        # internet.
-        'networkInterfaces': [{
-            'network': 'global/networks/default',
-            'accessConfigs': [
-                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
-            ]
-        }],
-
-        # Allow the instance to access cloud storage and logging.
-        'serviceAccounts': [{
-            'email': 'default',
-            'scopes': [
-                'https://www.googleapis.com/auth/devstorage.read_write',
-                'https://www.googleapis.com/auth/logging.write'
-            ]
-        }],
-
-        # Metadata is readable from the instance and allows you to
-        # pass configuration from deployment scripts to instances.
-        'metadata': {
-            'items': [{
-                # Startup script is automatically executed by the
-                # instance upon startup.
-                'key': 'startup-script',
-                'value': startup_script
-            }, {
-                'key': 'url',
-                'value': image_url
-            }, {
-                'key': 'text',
-                'value': image_caption
-            }, {
-                'key': 'bucket',
-                'value': bucket
-            }]
-        }
-    }
-
-    return compute.instances().insert(
-        project=project,
-        zone=zone,
-        body=config).execute()
-# [END create_instance]
-'''
